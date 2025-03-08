5a5e2a95f651b668c44bcbd27469f7c7fd2db463
vietlecd <120305631+vietlecd@users.noreply.github.com>
2025-03-08 14:13:02 +0700

    initial commit

---
+++ src/main/java/com/project/dadn/DadnApplication.java
@@ -0,0 +1,13 @@
+package com.project.dadn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadnApplication.class, args);
+	}
+
+}
