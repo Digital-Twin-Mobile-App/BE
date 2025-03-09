566f67d32c425bb50c5fb8b7d4ea1a62cc41b422
vietlecd <120305631+vietlecd@users.noreply.github.com>
2025-03-09 20:12:57 +0700

    unit test user

--- src/test/java/com/project/dadn/service/UserServiceTest.java
+++ src/test/java/com/project/dadn/service/UserServiceTest.java
@@ -10,10 +10,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -69,6 +71,16 @@ public class UserServiceTest {
         // WHEN
         var response = userService.createUser(request);
         // THEN
+
+        Assertions.assertThat(response.getUsername()).isEqualTo("john");
+    }
+
+    @Test
+    @WithMockUser(username = "john")
+    void getMyInfo_valid_success(){
+        when(userRepository.findByUsername(anyString())).thenReturn(Optional.of(user));
+
+        var response = userService.getMyInfo();
 
         Assertions.assertThat(response.getUsername()).isEqualTo("john");
     }
